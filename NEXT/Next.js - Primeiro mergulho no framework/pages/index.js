8bfe8967d82d6d13150f3f7de67f66666c115657
ceconcarlsen <ceconcarlsen@gmail.com>
2022-03-01 18:25:24 -0300

    feat: estilizando o projeto

--- NEXT/Next.js - Primeiro mergulho no framework/pages/index.js
+++ NEXT/Next.js - Primeiro mergulho no framework/pages/index.js
@@ -1,10 +1,33 @@
-import Link from "../src/components/Link";
+import HomeScreen from "../src/screens/HomeScreen";
 
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Alura Cases - Home</h1>
-      <Link href="/faq">Ir para o FAQ</Link>
-    </div>
-  );
-}
+export default HomeScreen;
+
+// import Link from "../src/components/Link";
+// import Footer from "../src/components/patterns/Footer";
+
+// const Title = ({ children, as }) => {
+//   const Tag = as;
+
+//   return (
+//     <>
+//       <Tag>{children}</Tag>
+//       <style jsx>
+//         {`
+//           ${Tag} {
+//             color: red;
+//           }
+//         `}
+//       </style>
+//     </>
+//   );
+// };
+
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <Title as="h1">Alura Cases - Home</Title>
+//       <Link href="/faq">Ir para o FAQ</Link>
+//       <Footer />
+//     </div>
+//   );
+// }
