7593434ea6ae52c4c4c24310f7a574ec0060f24e
ceconcarlsen <ceconcarlsen@gmail.com>
2022-03-01 16:49:20 -0300

    feat: Conhecendo Next.js

---
+++ NEXT/Next.js - Primeiro mergulho no framework/pages/faq.js
@@ -0,0 +1,12 @@
+import Link from "../src/components/Link";
+
+const FAQ = () => {
+  return (
+    <div>
+      <h1>Alura Cases - FAQ</h1>
+      <Link href="/">Ir para a HOME</Link>
+    </div>
+  );
+};
+
+export default FAQ;
